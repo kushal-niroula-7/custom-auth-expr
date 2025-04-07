4067b25b2f3cf4fc4c7adde1df865e06ce6238a6
kusal-niroula <kushal.niroula@wealthlane.co>
2025-04-07 08:36:47 +0545

    INitial

---
+++ AcerX/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcerX.Controllers
+{
+    public class UserController : Controller
+    {
+        // Action
+        public IActionResult Single()
+        {
+            // Get data from database
+            var user = new User();
+            user.Id = 45;
+            user.Name = "Something";
+            return View(user);
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AcerX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcerX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
