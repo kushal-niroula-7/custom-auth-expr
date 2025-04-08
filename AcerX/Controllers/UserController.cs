ff5b83c2eea9f2d7726664211d909d7be707e31f
kusal-niroula <kushal.niroula@wealthlane.co>
2025-04-08 08:37:11 +0545

    add

--- AcerX/Controllers/UserController.cs
+++ AcerX/Controllers/UserController.cs
@@ -13,11 +13,37 @@ namespace AcerX.Controllers
             user.Name = "Something";
             return View(user);
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(UserCreateVm vm)
+        {
+            try
+            {
+                var data = vm;
+                // Save to database
+                return RedirectToAction("Single");
+            }
+            catch(Exception ex)
+            {
+                return Redirect("/");
+            }
+        }
     }
 
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
+    }
+
+    public class  UserCreateVm
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
