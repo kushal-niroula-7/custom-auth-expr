34d74c541a7bd5acb75a5a73e3f10c08c31e1172
kusal-niroula <kushal.niroula@wealthlane.co>
2025-04-13 08:42:28 +0545

    changes

---
+++ AcerX/ConnectionProvider.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace AcerX;
+
+public static class ConnectionProvider
+{
+    public static IDbConnection GetDbConnection()
+    {
+        var connectionString = "Server=localhost;Port=5432;Database=first_run;User Id=postgres;Password=admin;";
+        return new NpgsqlConnection(connectionString);
+    }
+}
---
+++ AcerX/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using AcerX.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcerX.Controllers;
+
+public class ProductController : Controller
+{
+    public async Task<IActionResult> IndexAsync()
+    {
+        using var connection = ConnectionProvider.GetDbConnection();
+
+        var query = "select * from \"Products\" p";
+
+        var products = await connection.QueryAsync<Product>(query);
+
+        return View(products);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        using var connection = ConnectionProvider.GetDbConnection();
+
+        var query = $"select * from \"Products\" p WHERE \"Id\" = @paramId";
+
+        var product = await connection.QueryFirstOrDefaultAsync<Product>(query, new
+        {
+            paramId = id
+        });
+
+        return View(product);
+    }
+
+
+}
